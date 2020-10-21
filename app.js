d15bac70c195342b6192a8590bd093ea5c7b6bac
Mesfer <mesfer.vip@gmail.com>
2020-10-21 13:03:39 +0300

    create addNum method

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const app = Vue.createApp({
-
+    data(){
+       return{
+        counter:0,
+       };
+    },
+    methods:{
+       addNum(num){
+           this.counter = this.counter + num;
+       },
+    }
 });
 app.mount('#assignment');
