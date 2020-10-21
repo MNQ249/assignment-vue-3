118e95fec5dc25f37e34d2b1077b7a1479bdb265
Mesfer <mesfer.vip@gmail.com>
2020-10-21 15:14:50 +0300

    create two methods computed and watch

--- app.js
+++ app.js
@@ -4,13 +4,27 @@ const app = Vue.createApp({
         counter:0,
        };
     },
+    watch:{
+        result(){
+            const that = this;
+            setTimeout(function(){
+                that.counter = 0;
+            }, 5000)
+        }
+    },
+    computed:{
+        result(){ 
+            if (this.counter < 37){
+            return "Not there yet"; 
+        }else if(this.counter === 37){
+            return this.counter;
+        }else{
+            return "Too much!"; 
+        }
+    }
+    },
     methods:{
        addNum(num){
-           if (this.counter < 37){
-               console.log("Not there yet"); 
-           }else{
-               console.log("Too much!"); 
-           }
            this.counter = this.counter + num;
        },
     }
